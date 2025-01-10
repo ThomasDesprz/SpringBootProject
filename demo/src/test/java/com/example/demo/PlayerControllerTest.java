fa3f06f0a83161474ab3d5184cf8ae423b5dd8ca
ThomasDesprz <thomas.desprez50840@outlook.fr>
2025-01-10 20:35:31 +0100

    added a test report repository

--- demo/src/test/java/com/example/demo/PlayerControllerTest.java
+++ demo/src/test/java/com/example/demo/PlayerControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PlayerController.class)
+@ActiveProfiles("test")
 public class PlayerControllerTest {
 
     @Autowired
--- demo/src/test/java/com/example/demo/TeamControllerTest.java
+++ demo/src/test/java/com/example/demo/TeamControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(TeamController.class)
+@ActiveProfiles("test")
 public class TeamControllerTest {
 
     @Autowired
